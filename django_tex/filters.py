2b0a9b06a8ea4ff2d9a3938eb72c9b41c8f672f7
Carli* Freudenberg <kound@posteo.de>
2021-04-02 16:46:40 +0200

    make sure everything is escaped correctly

--- django_tex/filters.py
+++ django_tex/filters.py
@@ -1,27 +1,50 @@
 from django.utils.formats import localize_input
 from django.template.defaultfilters import register
 
-
-def do_linebreaks(value):
-    return value.replace('\n', '\\\\\n')
+BACKSLASH_REPLACEMENT = "<:-!~#BACKSLASH#~!:->"
 
 
-def do_latex_escape(value):
-    return (value
-        .replace('&', '\\&')
-        .replace('$', '\\$')
-        .replace('%', '\\%')
-        .replace('#', '\\#')
-        .replace('_', '\\_')
-        .replace('{', '\\{')
-        .replace('}', '\\}')
-        )
+def do_linebreaks(value: str) -> str:
+    return value.replace("\n", "\\\\\n")
+
+
+def do_latex_escape(value: str) -> str:
+    """
+    Replace all LaTeX characters that could cause the latex compiler to fail
+    and at the same time try to display the character as intended from the user.
+
+    see also https://tex.stackexchange.com/questions/34580/escape-character-in-latex
+    """
+
+    return (
+        # Replace Backslashes so we dont' double replace backslashes
+        value.replace("\\", BACKSLASH_REPLACEMENT)
+        # If there was a space before the character we assume the user wants
+        # it also in the displayed LaTeX text
+        .replace(BACKSLASH_REPLACEMENT + " ", r"\textbackslash\space ")
+        # Otherwise we assume it doesn't matter. Note the space at the end is
+        # required that an additional text is not considered as part of the latex
+        # command
+        .replace(BACKSLASH_REPLACEMENT, r"\textbackslash ")
+        # Do the same for ~ and ^
+        .replace("~ ", r"\textasciitilde\space ")
+        .replace("~", r"\textasciitilde ")
+        .replace("^ ", r"\textasciicircum\space ")
+        .replace("^", r"\textasciicircum ")
+        .replace("&", r"\&")
+        .replace("$", r"\$")
+        .replace("%", r"\%")
+        .replace("#", r"\#")
+        .replace("_", r"\_")
+        .replace("{", r"\{")
+        .replace("}", r"\}")
+    )
 
 
 tex_specific_filters = {
-    'localize': localize_input,
-    'linebreaks': do_linebreaks,
-    'latex_escape': do_latex_escape,
+    "localize": localize_input,
+    "linebreaks": do_linebreaks,
+    "latex_escape": do_latex_escape,
 }
 
 FILTERS = register.filters.copy()
